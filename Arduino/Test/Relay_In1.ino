eb92eb670eb1cfc5dd063455e11e5c67f260be46
Natalia <natalia.pavlovic@ucalgary.ca>
2018-02-11 19:26:13 -0700

    Update Relay_In1.ino

--- Arduino/Test/Relay_In1.ino
+++ Arduino/Test/Relay_In1.ino
@@ -1,3 +1,7 @@
+// This code is untested. I think the relay is Active Low (unsure of this). 
+// If this is the case, when HIGH is written, the relay will be open
+// and when LOW is written, the relay will be closed.
+
 int in1 = 7;
 void setup() {
   pinMode(in1, OUTPUT);
