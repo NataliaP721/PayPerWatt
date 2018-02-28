e6309a4d42bc72576fde2cbef5c26fee7499f9cf
Natalia <natalia.pavlovic@ucalgary.ca>
2018-02-27 22:03:50 -0700

    Update Relay_In1.ino

--- Arduino/Test/Relay_In1.ino
+++ Arduino/Test/Relay_In1.ino
@@ -1,10 +1,10 @@
 // CLOSE and OPEN were defined to make things easier.
-// The relay input is pin 7. 
+// The relay input is pin 13. 
 
 #define CLOSE LOW
 #define OPEN HIGH
 
-int in1 = 7;
+int in1 = 13;
 void setup() {
   pinMode(in1, OUTPUT);
   digitalWrite(in1, CLOSE);
