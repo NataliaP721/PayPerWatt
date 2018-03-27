5f3654d4115676cfa9c5f34faecdbdd8632177e0
Aysha Panatch <apanatch@MacBook-Pro.local>
2018-03-27 13:54:48 -0600

    Final App Version 1

--- Arduino/MasterArduinoCode/PayPerWatt/LED.ino
+++
@@ -1,9 +0,0 @@
-#include "PayPerWatt.h"
-
-void LED(){  // put your main code here, to run repeatedly:
-  if(charging==true)
-    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  else
-    digitalWrite(ledPin, LOW);   // turn the LED on (HIGH is the voltage level)
-}
-
--- Server/django_project/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-
-from . import views
-
-urlpatterns = [
-    url(r'^post/', views.post, name="post"),
-    url(r'^', views.get, name='get'),
-]
